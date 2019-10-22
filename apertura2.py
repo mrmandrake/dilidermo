d913a591bc1e8a8e1efa775b236932be38876963
MaDilo15 <56680603+MaDilo15@users.noreply.github.com>
2019-10-22 19:11:54 +0200

    Add files via upload

--- apertura2.py
+++ apertura2.py
@@ -4,8 +4,16 @@
 
         #Importo moduli: OS, OpenCV, Numpy
 
-import os, cv2, numpy
-
+import os, cv2, numpy, numpy.ma
+from matplotlib import pyplot as plt
+############################################################
+#Funzione di preprocessing immagini
+############################################################
+def Preprocess(img):
+        ret,img1 = cv2.threshold(img,127,255,cv2.TRESH_BINARY)
+        Hist=cv2.calcHist([img1.astype('float32')], channels=[2], mask=(), histSize=[img.size], ranges=[0,img.size])
+        cv2.imshow("Istograma",Hist)
+        return (Hist)
         #Controllo directory di lavoro. 
 mypath=os.getcwd()  #Attualmente sto lavorando sul Desktop
 newfolder=[]
@@ -18,19 +26,36 @@ for nome in os.listdir(mypath): #Elenco di cartelle presenti nella CDirectory
 ####################################################
 #Ingresso nella directory designata
 ####################################################
-             
+imgs=''             
 for name in newfolder:
  #i print sono di controllo funzionamento poi spariranno
     path=os.path.join(os.getcwd(), str(name))#controllo su ogni nome di folder, unisco alla cwd
     files=os.listdir(os.path.join(mypath, name))#vedo i file contenuti dentro ogni directory
     for fi in files:
             while fi.endswith('jpg'):
+                imgs=imgs+fi
+                imgs=imgs+'\n'
                 print(fi)
                 percorso=path
                 break
                 break
                 break
 print(percorso)
-
+os.chdir(percorso)
 ####################################################
-            
+#Apertura sequenziale immagini
+####################################################
+img_list = imgs.strip('][').split('\n')
+for k in img_list:
+        #if k!='':      controllo eccessivo, non serve tanto non andranno aperte
+                img_path=os.path.join(os.getcwd(),str(k))
+                img=cv2.imread(str(img_path))
+                cv2.imshow("Maledetta immagine",img)
+                Preprocess(img)
+                cv2.waitKey(0)  
+                cv2.destroyAllWindows()
+
+######################################################
+#Preprocessing
+######################################################
+
