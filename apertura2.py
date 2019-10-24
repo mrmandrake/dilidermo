f6f1da1bd2a59df4d5256b6a387f33d9c1ece827
MaDilo15 <56680603+MaDilo15@users.noreply.github.com>
2019-10-24 11:33:57 +0200

    Add files via upload
    Ciao Manuel, ho iniziato a definire le prime operazioni di preprocessing tra cui due gradienti di Sobel (X e Y) per cercare di definire l'oggetto pelo e successivamente eliminarlo se presente all'interno dell'oggetto neo/melanoma da analizzare attraverso la sostituzione del pixel con un altro del valore pari a quello della media di un kernel di 9-12 pixel nell'intorno di tale pixel.
    Attualmente il mio problema è che il codice non mi riconosce una conversione da float64 a unit8, dicendomi che numpy non possiede l'attributo unit8. Su internet ho trovato il contrario, magari sbaglio io

--- apertura2.py
+++ apertura2.py
@@ -4,16 +4,55 @@
 
         #Importo moduli: OS, OpenCV, Numpy
 
-import os, cv2, numpy, numpy.ma
+import os, cv2
+import numpy, numpy.ma
+import matplotlib
 from matplotlib import pyplot as plt
+
 ############################################################
-#Funzione di preprocessing immagini
+                #Calcolo istrogramma 
 ############################################################
-def Preprocess(img):
-        ret,img1 = cv2.threshold(img,127,255,cv2.TRESH_BINARY)
-        Hist=cv2.calcHist([img1.astype('float32')], channels=[2], mask=(), histSize=[img.size], ranges=[0,img.size])
-        cv2.imshow("Istograma",Hist)
-        return (Hist)
+def Histogram (grey, mask=None):
+        channel=[0]
+        hist=cv2.calcHist([grey], channels=[0], mask=None, histSize=[256], ranges=[0,256])
+        plt.plot(hist) 
+        plt.show()
+        return (hist)
+############################################################
+        
+############################################################
+#Funzione di preprocessing immagini (Calcolo gradienti)
+############################################################
+def Gradienti (img, grey):
+        laplacian64=cv2.Laplacian(img, cv2.CV_64F)
+        laplacian=numpy.unit8(numpy.absolute(laplacian64))
+        sobelx64 = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=5)
+        sbobelx=numpy.unit8(numpy.absolute(sobelx64))
+        sobely64 =cv2.Sobel(img,cv2.CV_64F, 0,1, ksize=5)
+        sobely=numpy.unit8(numpy.absolute(sobely64))
+        
+
+        matplotlib.pyplot.subplot(2,2,1)
+        matplotlib.pyplot.imshow(grey, cmap='gray')
+        matplotlib.pyplot.title('Original')
+        matplotlib.pyplot.xticks([])
+        matplotlib.pyplot.yticks([])
+        matplotlib.pyplot.subplot(2,2,2)
+        matplotlib.pyplot.imshow(laplacian,cmap='gray')
+        matplotlib.pyplot.title('Laplacian')
+        matplotlib.pyplot.xticks([])
+        matplotlib.pyplot.yticks([])
+        matplotlib.pyplot.subplot(2,2,3)
+        matplotlib.pyplot.imshow(sobelx,cmap='gray')
+        matplotlib.pyplot.title('Sobel X')
+        matplotlib.pyplot.xticks([])
+        matplotlib.pyplot.yticks([])
+        matplotlib.pyplot.subplot(2,2,4)
+        matplotlib.pyplot.imshow(sobely,cmap='gray')
+        matplotlib.pyplot.title('Sobel Y')
+        matplotlib.pyplot.xticks([])
+        matplotlib.pyplot.yticks([])
+        matplotlib.pyplot.show()
         #Controllo directory di lavoro. 
 mypath=os.getcwd()  #Attualmente sto lavorando sul Desktop
 newfolder=[]
@@ -47,15 +86,25 @@ os.chdir(percorso)
 ####################################################
 img_list = imgs.strip('][').split('\n')
 for k in img_list:
-        #if k!='':      controllo eccessivo, non serve tanto non andranno aperte
+        if k!='':      #controllo eccessivo, non serve tanto non andranno aperte
                 img_path=os.path.join(os.getcwd(),str(k))
                 img=cv2.imread(str(img_path))
-                cv2.imshow("Maledetta immagine",img)
-                Preprocess(img)
-                cv2.waitKey(0)  
+                grey=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+                cv2.imshow("Maledetta immagine",grey)
+                Histogram(grey)
+                Gradienti (img, grey)
+                cv2.waitKey(500)  
                 cv2.destroyAllWindows()
 
 ######################################################
 #Preprocessing
 ######################################################
+
+
+
+
+
+
+
+
 
