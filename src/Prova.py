b35965e4f6f85817a07ca1c756b69ebb2d556b3a
MaDilo15 <matteo.diliberto.ca@gmail.com>
2019-11-06 12:55:41 +0100

    C'è una bozza di digital shaving, non riesco a capire il problema con il parametro x lin:80.

--- src/Prova.py
+++ src/Prova.py
@@ -17,7 +17,7 @@ class Folder:
                 if names.endswith("2017"):
                     perc=os.path.join(mypath,str(pths),str(names))
                     os.chdir(str(perc))
-                    return(str(perc))
+                    return(perc)
                     break
                 break
                 
@@ -31,18 +31,70 @@ class Folder:
                     immagine=cv2.imread(perc_img)
                     cv2.imshow('immagine',immagine)
                     cv2.waitKey(0)  
-                    cv2.destroyAllWindows()
-                return(immagine)
+                    #cv2.destroyAllWindows()
+                    return(immagine)
                     
                     
 
 class Preprocessing:
+
     def Histograms(immagine):
         channels=[0]
         hist=cv2.calcHist([immagine], channels, mask=None, histSize=[256], ranges=[0,256])
         plt.plot(hist) 
         plt.show()
         return (hist)
+        
+    def Contrasto (immagine):
+        img=immagine
+        lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        #cv2.imshow("lab",lab)
+
+        #-----Splitting the LAB image to different channels-------------------------
+        l, a, b = cv2.split(lab)
+        #cv2.imshow('l_channel', l)
+        #cv2.imshow('a_channel', a)
+        #cv2.imshow('b_channel', b)
+
+        #-----Applying CLAHE to L-channel-------------------------------------------
+        clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(4,4))
+        cl = clahe.apply(l)
+        cv2.imshow('CLAHE output', cl)
+
+        #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+        limg = cv2.merge((cl,a,b))
+        cv2.imshow('limg', limg)
+
+        #-----Converting image from LAB Color model to RGB model--------------------
+        finalContrast = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        cv2.imshow('final', finalContrast)
+        Preprocessing.RimPix(finalContrast)
+
+    def RimPix (finalContrast):
+        BW= cv2.cvtColor(finalContrast, cv2.COLOR_RGB2GRAY)
+        ret,BIN=cv2.threshold(BW,35,255,cv2.THRESH_BINARY)
+        cv2.imshow('BIN',BIN)
+        edged=cv2.Canny(BIN, 20, 30, apertureSize=5)
+        lines = cv2.HoughLines(edged,10,numpy.pi/180, 20,30,10)
+        for x in range(len(lines)):
+            for x1,y1,x2,y2 in lines[x]:
+                #cv2.line(inputImage,(x1,y1),(x2,y2),(0,128,0),2, cv2.LINE_AA)
+                pts = np.array([[x1, y1 ], [x2 , y2]], np.int32)
+                cv2.polylines(inputImage, [pts], True, (0,255,0))
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(inputImage,"Tracks Detected", (500, 250), font, 0.5, 255)
+                cv2.imshow("Trolley_Problem_Result", finalContrast)
+                cv2.imshow('edge', edged)
+                cv2.waitKey(0)
+                
+        #Preprocessing.Gradienti(BIN)
+    
+    #def Gradienti( immagine):
+        
+
+
+  
 
 
 mypath=os.getcwd()
@@ -51,5 +103,6 @@ prepro=Preprocessing()
 lista_cartelle=os.listdir(mypath)
 percorso=myfold.Cartella(mypath, lista_cartelle[4])
 immagine=myfold.Apertura(percorso)
-Preprocessing.Histograms(immagine)
+finalContrast=Preprocessing.Contrasto(immagine)
+Preprocessing.Histograms(finalContrast)
 
