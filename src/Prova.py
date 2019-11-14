8639825d30f1c2afa60e7652cd36bbb238dc9882
MaDilo15 <matteo.diliberto.ca@gmail.com>
2019-11-14 12:46:33 +0100

    Non riesco a creare SnakeCoord per creare lo snake

--- src/Prova.py
+++ src/Prova.py
@@ -3,7 +3,9 @@ import numpy, numpy.ma
 import matplotlib
 import sys
 from matplotlib import pyplot as plt
-import random 
+import random
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse_perimeter
 
 #Definisco la classe di ricerca della cartella delle immagini
 class Folder:
@@ -31,7 +33,7 @@ class Folder:
                     perc_img=os.path.join(phts,str(photos))
                     immagine=cv2.imread(perc_img)
                     cv2.imshow('immagine',immagine)
-                    cv2.waitKey(0)  
+                    #cv2.waitKey(0)  
                     #cv2.destroyAllWindows()
                     return(immagine)
                     
@@ -60,15 +62,12 @@ class Preprocessing:
         #-----Applying CLAHE to L-channel-------------------------------------------
         clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(4,4))
         cl = clahe.apply(l)
-        cv2.imshow('CLAHE output', cl)
 
         #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
         limg = cv2.merge((cl,a,b))
-        cv2.imshow('limg', limg)
 
         #-----Converting image from LAB Color model to RGB model--------------------
         finalContrast = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-        cv2.imshow('final', finalContrast)
         Preprocessing.Grads(finalContrast,immagine)
         #Preprocessing.RimPix(finalContrast,immagine)
     
@@ -76,7 +75,6 @@ class Preprocessing:
         kernel=(1,15)
         imm=immagine
         grads=cv2.morphologyEx(finalContrast, cv2.MORPH_GRADIENT, kernel)
-        cv2.imshow('Grads',grads)
         for Xcoord in range(0,grads.shape[0]):
             for Ycoord in range(0,grads.shape[1]):
                 pixs=grads[Xcoord,Ycoord]
@@ -84,20 +82,28 @@ class Preprocessing:
                 if pixs[0]>=50 & pixs[2]>=50:
                     if pixs[1] >=25:
                         imm[Xcoord,Ycoord]=[255,255,0]
-                        print("Trovato pixel")
-                        print(imm [Xcoord,Ycoord])
-        cv2.imshow("Primo Tentativo", imm)
+        Preprocessing.HairsPix(imm)
+        #Le Coordinate dei pixel non servono, vengono identificate su RimPix,
+        #Questa funzione di gradiente resta, ma andrà definita un'altra funzione
+        #Per l'identificazione della ROI contenente il Neo.
+
+                                
+
+    def HairsPix (imm):
+        #Da implementare: ho trovato le linee dei peli, ora devo creare un kernel, di grandezza variabile, che passi sui peli identificati e applichi
+        #una sostituzione delle intensità del valore dei peli con l'intensità gaussiana media delle intensità del neo meno i pixel dei peli. Se invece sono fuori dalla ROI,
+        #una loro sostituzione con un valore altamente identificabile (0,255,0).
         kernel_size = 5
-        blur_gray = cv2.GaussianBlur(immagine,(kernel_size, kernel_size),0)
+        blur_gray = cv2.GaussianBlur(imm,(kernel_size, kernel_size),0)
 
-        low_threshold = 50
-        high_threshold = 150
+        low_threshold = 25
+        high_threshold = 90
         edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-
+        cv2.imshow('canny', edges)
         rho = 1  # distance resolution in pixels of the Hough grid
         theta = numpy.pi / 180  # angular resolution in radians of the Hough grid
         threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 50  # minimum number of pixels making up a line
+        min_line_length = 30  # minimum number of pixels making up a line
         max_line_gap = 20  # maximum gap in pixels between connectable line segments
         line_image = numpy.copy(immagine) * 0  # creating a blank to draw lines on
 
@@ -105,8 +111,8 @@ class Preprocessing:
         # Output "lines" is an array containing endpoints of detected line segments
         lines = cv2.HoughLinesP(edges, rho, theta, threshold, numpy.array([]),
                     min_line_length, max_line_gap)
-        print(lines)
         points = []
+
         for line in lines:
             for x1, y1, x2, y2 in line:
                 points.append(((x1 + 0.0, y1 + 0.0), (x2 + 0.0, y2 + 0.0)))
@@ -114,37 +120,35 @@ class Preprocessing:
 
         lines_edges = cv2.addWeighted(immagine, 0.8, line_image, 1, 0)
         cv2.imshow('Linee', lines_edges)
-                                
-
-    def RimPix (imm):
-        BW= cv2.cvtColor(imm, cv2.COLOR_RGB2GRAY)
-        ret,BIN=cv2.threshold(BW,35,255,cv2.THRESH_BINARY)
-        cv2.imshow('BIN',BIN)
-        edged=cv2.Canny(BIN, 20, 30, apertureSize=5)
-        lines = cv2.HoughLines(edged,10,numpy.pi/180, 255,255,0)
-        for x1 in lines:
-                #cv2.line(inputImage,(x1,y1),(x2,y2),(0,128,0),2, cv2.LINE_AA)
-                if flg==1:
-                    pts = numpy.array([[x, y]], numpy.int32)
-                    flg=flg+1
-                else:
-                    pts2=numpy.array([[x,y]], numpy.int32)
-                    pts=numpy.concatenate((pts, pts2))
-                    cv2.polylines(immagine, [pts], True, (0,255,0))
-                    flg=1
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(immagine,"Tracks Detected", (500, 250), font, 0.5, 255)
-        cv2.imshow("Trolley_Problem_Result", immagine)
-        cv2.imshow('edge', immagine)
-        
-
-            
-                
-        #Preprocessing.Gradienti(BIN)
+        Preprocessing.NeoContours(imm, lines_edges)
     
-    #def Gradienti( immagine):
+    def NeoContours(immagine, lines_edges):
+        # Convert to grayscale
+        gray = cv2.cvtColor(immagine, cv2.COLOR_BGR2GRAY)
+        _, gray = cv2.threshold(gray, 100, 125, cv2.THRESH_BINARY)
+
+        # Downsize image (by factor 4) to speed up morphological operations
+        #gray = cv2.resize(gray, dsize=(0, 0), fx=0.25, fy=0.25)
+
+        # Morphological Closing: Get rid of the hole
+        gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10)))
+
+        # Morphological opening: Get rid of the stuff at the top of the circle
+        gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (40,40)))
+
+        # Resize image to original size
+        gray = cv2.resize(gray, dsize=(immagine.shape[1], immagine.shape[0]))
+
+        # Find contours (only most external)
+        cnts, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
+
+        # Draw found contour(s) in input image
+        image = cv2.drawContours(immagine, cnts, -1, (0, 0, 255), 2)
+
+        cv2.imshow('Contours', image)
+
         
+
 
 
   
---
+++ src/Prova2.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import cv2
+import os
+import glob
+import sys
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse_perimeter
+from skimage.segmentation import active_contour
+
+class IMG_list:
+    def __init__(self, path):
+        self.path = path
+
+
+    def List(path):
+        img_list=[]
+        img_paths=[]
+        for r,d, f in os.walk(path):
+            for names in d:
+                if names.endswith('img'):
+                    for root, dir, files in os.walk(names):
+                        if dir !=[]:
+                            os.chdir(os.path.join(str(path),str(names), str(dir[0])))
+                            path=os.getcwd()             
+                            for r,d,f in os.walk(path):
+                                for files in f:
+                                    if files.endswith('.bmp'):
+                                        img_list.append(os.path.join(str(r),str(files)))
+        return (img_list)
+
+class Preprocessing:
+    def PrePro(img):
+        cv2.imshow('immagine',img)
+        cv2.waitKey(0)  
+        cv2.destroyAllWindows()
+        image = img.copy()
+        # blur image
+        image = cv2.GaussianBlur(image, (5, 5), 0)
+        # Applying CLAHE to resolve uneven illumination
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        hsv[:, :, 2] = clahe.apply(hsv[:, :, 2])
+        image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        kernel = np.ones((11, 11), np.uint8)
+        for i in range(image.shape[-1]):
+            image[:, :, i] = cv2.morphologyEx(
+            image[:, :, i],
+            cv2.MORPH_CLOSE, kernel)
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        cv2.imshow('hsv', hsv_image)
+        return (hsv_image)
+                
+    def Lines(img,Open):
+        SnakeCoord=[]
+        colour=(10,10,10)
+        for x in range (Open.shape[0]):
+            for y in range(Open.shape[1]):
+                r,g,b=Open[x,y]
+                if r >= colour[0] & g>=colour[1] & b>=colour[2]:
+                    SnakeCoord=SnakeCoord.append(x)
+                    SnakeCoord=SnakeCoord.append(y)
+                    
+        
+        SnakeCoord=np.array(SnakeCoord)
+        contour=active_contour(Open, SnakeCoord, alpha=0.8, beta=0.7, w_line= -0.7, w_edge=0.5,
+                                gamma=0.02,
+                                bc=None,
+                                max_px_move=10,
+                                max_iterations=3000)
+   
+
+
+
+
+path=os.getcwd()
+mystring=IMG_list.List(path)
+processing=Preprocessing()
+iterations=3
+for names in mystring:
+    img=cv2.imread(names)
+    Open=Preprocessing.PrePro(img)
+    gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    Preprocessing.Lines(img, Open)
+
